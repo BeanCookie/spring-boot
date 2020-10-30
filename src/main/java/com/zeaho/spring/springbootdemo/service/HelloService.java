d16f1d259fae538063625c09c2ee5ba7756cedf6
luzhong <luzhong@zeaho.com>
2020-10-30 11:12:06 +0800

    feature(service): 添加自定义的starter项目
    1. 通过自动配置获取Bean

--- src/main/java/com/zeaho/spring/springbootdemo/service/HelloService.java
+++ src/main/java/com/zeaho/spring/springbootdemo/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.zeaho.spring.springbootdemo.service;
 
+import com.zeaho.spring.service.TranslateService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,7 +8,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HelloService {
+    private final TranslateService translateService;
+
+    public HelloService(TranslateService translateService) {
+        this.translateService = translateService;
+    }
+
     public String sayHello(String name) {
-        return String.format("hello world! hello %s!", name);
+        return String.format("hello world! hello %s!", translateService.translate(name));
     }
 }
