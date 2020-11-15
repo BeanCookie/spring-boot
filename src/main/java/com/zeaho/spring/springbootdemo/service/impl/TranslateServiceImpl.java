f75f83c40856c10728e6e7c8b614a7e81dcb0df9
LuZhong <724821454@qq.com>
2020-11-15 11:41:24 +0800

    feat(service,resources): 添加自定义配置
    1. 使用@ConfigurationProperties封装配置

--- src/main/java/com/zeaho/spring/springbootdemo/service/impl/TranslateServiceImpl.java
+++ src/main/java/com/zeaho/spring/springbootdemo/service/impl/TranslateServiceImpl.java
@@ -1,8 +1,10 @@
 package com.zeaho.spring.springbootdemo.service.impl;
 
+import com.zeaho.spring.springbootdemo.properties.YouDaoTranslateProperties;
 import com.zeaho.spring.springbootdemo.service.TranslateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +14,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class TranslateServiceImpl implements TranslateService {
     private final Logger logger = LoggerFactory.getLogger(TranslateServiceImpl.class);
-    @Value("${appKey}")
-    private String appKey;
+    private final YouDaoTranslateProperties youDaoTranslateProperties;
 
-    @Value("${appSecret}")
-    private String appSecret;
+    public TranslateServiceImpl(YouDaoTranslateProperties youDaoTranslateProperties) {
+        this.youDaoTranslateProperties = youDaoTranslateProperties;
+    }
 
-    @Value("${youdaoUrl}")
-    private String youdaoUrl;
     @Override
     public String translate(String text) {
-        logger.info("{} {} {}", appKey, appSecret, youdaoUrl);
+        logger.info("{} {} {}", youDaoTranslateProperties.getAppKey(), youDaoTranslateProperties.getAppSecret(), youDaoTranslateProperties.getYoudaoUrl());
         return String.format("translate: %s", text);
     }
 }
