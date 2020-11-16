a9d5ed80537a7b71739b41f7a4c6687a41346ce8
luzhong <luzhong@zeaho.com>
2020-11-16 17:20:26 +0800

    feature(controller,service): 去除自定义配置，使用自定义的starter项目

--- src/main/java/com/zeaho/spring/springbootdemo/controller/HelloController.java
+++ src/main/java/com/zeaho/spring/springbootdemo/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.zeaho.spring.springbootdemo.controller;
 
 import com.zeaho.spring.springbootdemo.service.HelloService;
-import com.zeaho.spring.springbootdemo.service.TranslateService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
     private final HelloService helloService;
-    private final TranslateService translateService;
 
-    public HelloController(HelloService helloService, TranslateService translateService) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.translateService = translateService;
     }
 
     @GetMapping("/world")
     public String world(@RequestParam("name") String name) {
-        return helloService.sayHello(translateService.translate(name));
+        return helloService.sayHello(name);
     }
 }
